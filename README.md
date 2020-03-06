00e62922c4351c2fcb593c55bda10f1b96ace519
Pedro Filho <pedro@filho.me>
2020-03-06 16:54:00 +0000

    adding script and instructions to readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# devops-nightmare
-Call your devops engineer whenever the server is down
+# DevOps Nightmare
+
+Call your DevOps engineer whenever the server is down
+
+You'll need a Twilio account and these keys on your environment:
+
+```
+ACCOUNT_SID='###'
+AUTH_TOKEN='###'
+
+FROM_NUMBER='###'
+DEVOPS_NUMBER='###'
+
+HEALTH_CHECK='###'
+
+BOSS_VOICE_URL = '###
+```
+
+It'll ping the health_check endpoint each 5 seconds, if it fails 3 times, then it calls your DevOps engineer with the recording of your boss.
---
+++ call_devops.py
@@ -0,0 +1,37 @@
+from twilio.rest import Client
+from time import sleep
+from requests import get
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+account_sid = os.getenv('ACCOUNT_SID')
+auth_token = os.getenv('AUTH_TOKEN')
+
+from_number = os.getenv('FROM_NUMBER')
+devops_number = os.getenv('DEVOPS_NUMBER')
+
+health_check = os.getenv('HEALTH_CHECK')
+
+boss_voice_url = os.getenv('BOSS_VOICE_URL')
+
+client = Client(account_sid, auth_token)
+
+failed_requests = 0
+
+while (failed_requests <= 3):
+    r = get(health_check)
+    status = r.json()
+    if(status == "OK"):
+        print("FUCK")
+        failed_requests += 1
+    else:
+        print("OK")
+    sleep(5)
+
+call = client.calls.create(
+    url=boss_voice_url,
+    to=devops_number,
+    from_=from_number
+)
